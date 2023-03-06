5219184b23cd96baf9070923d2ae1644958bc283
Paweł <pawel@MBP-Pawel.local>
2023-03-06 07:10:44 +0100

    added script that generates config from components

---
+++ scripts/generate_config.py
@@ -0,0 +1,27 @@
+from spacy.cli import init_config
+from thinc.api import Config
+from pathlib import Path
+import typer
+
+def main(
+    base_params: Path           = typer.Argument(..., help="Path to base config parameters"),
+    non_default_params: Path    = typer.Argument(..., help="Path to extended config parameters"),
+    output: Path                = typer.Argument(..., help="Path to save resulting config")
+) -> None:
+    base_params = Config().from_disk(base_params)['base_params']
+
+    base_config = init_config(
+        lang        = base_params['lang'],
+        pipeline    = base_params['pipeline'],
+        optimize    = base_params['optimize'],
+        gpu         = base_params['gpu'],
+        pretraining = base_params['pretraining']
+    )
+
+    extended_config = Config().from_disk(path=non_default_params, interpolate=False)
+    final_config = Config(base_config).merge(extended_config)
+    final_config.to_disk(output)
+
+if __name__ == '__main__':
+    typer.run(main)
+
