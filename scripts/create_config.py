a17031bd99bb3108fe2e6e21dac702abc27073c9
p-olejnik <pw.olejnik@student.uw.edu.pl>
2023-03-06 07:45:14 +0100

    Added fill_config to script for result validation

--- scripts/create_config.py
+++ scripts/create_config.py
@@ -1,4 +1,4 @@
-from spacy.cli import init_config
+from spacy.cli import init_config, fill_config
 from thinc.api import Config
 from pathlib import Path
 import typer
@@ -21,6 +21,9 @@ def main(
     extended_config = Config().from_disk(path=non_default_params, interpolate=False)
     final_config = Config(base_config).merge(extended_config)
     final_config.to_disk(output)
+
+    # Make sure that the final config is complete. If not, fill it with default values
+    fill_config(output, output, pretraining=base_params['pretraining])
 
 if __name__ == '__main__':
     typer.run(main)
